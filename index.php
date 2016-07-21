1c048081ccf594dd1f296de2ad568fbc6bd621e7
sasor <s.leamsi99@gmail.com>
2016-07-21 09:51:00 -0400

    css agregado, parte visual terminada... no quedo tan bonitogit status

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+		<title>Todo PhP</title>
+		<link rel="stylesheet" href="css/style.css"/>
+	</head>
+	<body class="no-margin no-padding">
+		<main class='skel'>
+			<h1 class='skel_title no-margin no-padding'>To do</h1>	
+			<nav class='skel_nav'>
+				<li class='nav_item'>
+					<span class='none'>third</span>
+					<i class='i'>+</i>
+					<a href="#0">Mark as done</a>
+				</li>
+				<li class='nav_item'>
+					<span class='done'>second</span>
+					<i class='i'>+</i>
+					<a href="#0">Mark as done</a>
+				</li>
+				<li class='nav_item'>
+					<span class='none'>terminar la tarea de leo</span>
+					<i class='i'>+</i>
+					<a href="#0">Mark as done</a>
+				</li>
+				<li class='nav_item'>
+					<span class='none'>second</span>
+					<i class='i'>+</i>
+					<a href="#0">Mark as done</a>
+				</li>
+				<li class='nav_item'>
+					<span class='none'>third</span>
+					<i class='i'>+</i>
+					<a href="#0">Mark as done</a>
+				</li>
+			</nav>
+			<form class='skel_action' method="GET">
+				<input type="text" placeholder="add task" autocomplete="off" required />
+				<input type="submit" value="Taskel"/>
+			</form>
+		</main>
+	</body>
+</html>
